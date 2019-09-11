59eb4985260503528a4ea131c0ae9aa55aba371f
Bluebird2000 <ahmad.saka@photizzo.ng>
2019-09-11 16:41:47 +0100

    switch app baseUrl from local to server

---
+++ constants/images.js
@@ -0,0 +1,11 @@
+export const background = {
+    welcome: require("../assets/images/slider-two.png"),
+    encrypted: require("../assets/images/slider-one.png"),
+    privacy: require("../assets/images/splash-three.png")
+  };
+  
+  export default {
+    background,
+    icons
+  };
+  
